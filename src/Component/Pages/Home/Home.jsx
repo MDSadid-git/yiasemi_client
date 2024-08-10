f1b140628617d80d92aeb2f53f74aa6605fcab90
MD Sadid <sfhsadid@gmail.com>
2024-08-10 06:28:12 +0300

    Featured area done

--- src/Component/Pages/Home/Home.jsx
+++ src/Component/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from './Banner/Banner';
 import FoodCategory from './FoodCategory/FoodCategory';
 import PopluerMenu from './PopluerMenu/PopluerMenu';
+import Featured from './Featured/Featured';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner />
             <FoodCategory />
             <PopluerMenu />
+            <Featured />
         </div>
     );
 };
