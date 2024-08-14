8345306ad19dbfc35b56f5912a734f2e983574e8
MD Sadid <sfhsadid@gmail.com>
2024-08-14 09:54:26 +0300

    Page cover section area done

--- src/Component/Pages/Menu/Menu.jsx
+++ src/Component/Pages/Menu/Menu.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
-
+import PageCover from "../Shared/PageCover/PageCover";
+import sectionImageHeader from "../../Assets/menu/banner3.jpg";
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>Yiasemi Lounge \ Menu</title>
       </Helmet>
-      Menu
+      <PageCover coverImg={sectionImageHeader} />
     </div>
   );
 };
--- src/Component/Pages/Shared/PageCover/PageCover.jsx
+++ src/Component/Pages/Shared/PageCover/PageCover.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const PageCover = () => {
-  return <div>Page Cover</div>;
+const PageCover = ({ coverImg }) => {
+  return (
+    <div>
+      <div
+        className="hero md:h-[600px] h[400px]"
+        style={{
+          backgroundImage: `url(${coverImg})`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="flex justify-center md:pt-36 pt-5 text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PageCover;
