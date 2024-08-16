d5a9805044e126e780d9bea882d410bbf5e2aa60
MD Sadid <sfhsadid@gmail.com>
2024-08-16 07:08:44 +0300

    all menu data featch done

--- src/Component/Pages/Menu/Menu.jsx
+++ src/Component/Pages/Menu/Menu.jsx
@@ -3,7 +3,16 @@ import { Helmet } from "react-helmet-async";
 import PageCover from "../Shared/PageCover/PageCover";
 import sectionImageHeader from "../../Assets/menu/banner3.jpg";
 import PopluerMenu from "../Home/PopluerMenu/PopluerMenu";
+import useMenu from "../../Hooks/useMenu";
 const Menu = () => {
+  const [menu] = useMenu();
+  const PopluerMenu = menu.filter((item) => item.category === "popular");
+  const dessert = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+
   return (
     <div>
       <Helmet>
