aac7425e10a7525fda475ee4708d8f1ed8ef3629
MD Sadid <sfhsadid@gmail.com>
2024-08-16 13:38:04 +0300

    menuCategory page Page cover done

--- src/Component/Pages/Menu/Menu.jsx
+++ src/Component/Pages/Menu/Menu.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 import PageCover from "../Shared/PageCover/PageCover";
 import sectionImageHeader from "../../Assets/menu/banner3.jpg";
-import PopluerMenu from "../Home/PopluerMenu/PopluerMenu";
+// import PopluerMenu from "../Home/PopluerMenu/PopluerMenu";
 import useMenu from "../../Hooks/useMenu";
 const Menu = () => {
   const [menu] = useMenu();
   const PopluerMenu = menu.filter((item) => item.category === "popular");
-  const dessert = menu.filter((item) => item.category === "dessert");
-  const soup = menu.filter((item) => item.category === "soup");
-  const salad = menu.filter((item) => item.category === "salad");
-  const pizza = menu.filter((item) => item.category === "pizza");
-  const offered = menu.filter((item) => item.category === "offered");
+  // const dessert = menu.filter((item) => item.category === "dessert");
+  // const soup = menu.filter((item) => item.category === "soup");
+  // const salad = menu.filter((item) => item.category === "salad");
+  // const pizza = menu.filter((item) => item.category === "pizza");
+  // const offered = menu.filter((item) => item.category === "offered");
 
   return (
     <div>
@@ -23,7 +23,7 @@ const Menu = () => {
         menuTitle={"Our Menu"}
         menuDesctription={"Would You Like to Try A Dish"}
       />
-      <PopluerMenu />
+      {/* <PopluerMenu /> */}
     </div>
   );
 };
--- src/Component/Pages/Shared/MenuCategory/MenuCategory.jsx
+++ src/Component/Pages/Shared/MenuCategory/MenuCategory.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import PageCover from "../PageCover/PageCover";
 
 const MenuCategory = () => {
-  return <div></div>;
+  return (
+    <div>
+      <PageCover
+        coverImg={img}
+        menuTitle={menuTitle ? menuTitle : ""}
+        menuDesctription={menuDesctription ? menuDesctription : ""}
+      />
+    </div>
+  );
 };
 
 export default MenuCategory;
