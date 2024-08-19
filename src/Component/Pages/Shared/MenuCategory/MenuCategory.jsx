40ef3067782fa745300e35cfe5c787d470960516
MD Sadid <sfhsadid@gmail.com>
2024-08-19 09:28:52 +0300

    order page 86% done

--- src/Component/Pages/Shared/MenuCategory/MenuCategory.jsx
+++ src/Component/Pages/Shared/MenuCategory/MenuCategory.jsx
@@ -22,7 +22,7 @@ const MenuCategory = ({ allItem, img, menuDesctription, menuTitle }) => {
           </div>
           <div>
             <Link
-              to={"/order"}
+              to={`/order/${menuTitle}`}
               class="relative inline-flex my-5 items-center justify-center p-4 px-6 py-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-brand rounded-full shadow-md group"
             >
               <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-brand group-hover:translate-x-0 ease">
